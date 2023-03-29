6dc2edd301a728e8eabb1caf35fe335598f0e0e5
Riccardo March <sikkid47@gmail.com>
2023-03-29 23:35:00 +0200

    Patches

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,8 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("com.realgear:readable-bottom-bar:1.0.0")
 
-    //implementation(project(mapOf("path" to ":readable-bottom-bar")))
+    implementation(project(mapOf("path" to ":readable-bottom-bar")))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
